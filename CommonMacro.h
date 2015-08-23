7b4e71557ab1083d84ae693d26167414949a9d07
duxinfeng <duxinfeng99@gmail.com>
2015-08-23 13:18:42 +0800

    add Macro file

---
+++ CommonMacro.h
@@ -0,0 +1,59 @@
+//
+//  CommonMacro.h
+//  HaoJiaZhang
+//
+//  Created by duxinfeng on 15/8/22.
+//  Copyright (c) 2015年 好家长. All rights reserved.
+//
+
+#ifndef CommonMacro_h
+#define CommonMacro_h
+
+
+
+#define IS_IOS7 ([[UIDevice currentDevice].systemVersion doubleValue] >= 7.0)
+
+#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+
+
+#define SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+#define SCREEN_HEIGHT ([[UIScreen mainScreen] bounds].size.height)
+#define SCREEN_MAX_LENGTH (MAX(SCREEN_WIDTH, SCREEN_HEIGHT))
+#define SCREEN_MIN_LENGTH (MIN(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#define IS_IPHONE_4_OR_LESS (IS_IPHONE && SCREEN_MAX_LENGTH < 568.0)
+#define IS_IPHONE_5 (IS_IPHONE && SCREEN_MAX_LENGTH == 568.0)
+#define IS_IPHONE_6 (IS_IPHONE && SCREEN_MAX_LENGTH == 667.0)
+#define IS_IPHONE_6P (IS_IPHONE && SCREEN_MAX_LENGTH == 736.0)
+
+#define WIDTH self.view.frame.size.width
+#define HEIGHT self.view.frame.size.height
+
+//设置frame
+#define FRAME(x, y, width, height) CGRectMake(x, y, width, height)
+
+//计算当前高度、宽度
+#define NowHeight(a) (a.frame.origin.y + a.frame.size.height)
+#define NowWidth(a) (a.frame.origin.x + a.frame.size.width)
+
+
+/*颜色类*/
+#define HEXCOLOR(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+//带有RGBA的颜色设置
+#define COLOR(R, G, B, A) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
+
+// 获取RGB颜色
+#define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
+#define RGB(r,g,b) RGBA(r,g,b,1.0f)
+
+/* 打印 */
+//DEBUG  模式下打印日志,当前行
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#   define DLog(...)
+#endif
+
+#endif
